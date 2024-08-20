63f42f871db426b4670f52c1a6541ba61b055b06
0xKitsune <0xkitsune@protonmail.com>
2024-08-19 23:45:02 -0400

    added discover bin

---
+++ bin/discover.rs
@@ -0,0 +1,27 @@
+use std::sync::Arc;
+
+use alloy::providers::ProviderBuilder;
+
+use amms::discovery::factory::{discover_factories, DiscoverableFactory};
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    tracing_subscriber::fmt::init();
+
+    // Find all UniswapV2 and UniswapV3 compatible factories and filter out matches with less than 1000 AMMs
+    let number_of_amms_threshold = 1000;
+    let factories = discover_factories(
+        vec![
+            DiscoverableFactory::UniswapV2Factory,
+            DiscoverableFactory::UniswapV3Factory,
+        ],
+        number_of_amms_threshold,
+        provider,
+        50000,
+    )
+    .await?;
+
+    println!("Factories: {:?}", factories);
+
+    Ok(())
+}
